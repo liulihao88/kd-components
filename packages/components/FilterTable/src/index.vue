211c1323384cfad3d3cce5cd9a1057ff3713fa26
hrr <huruirui@keendata.com>
2022-08-04 11:16:56 +0800

    找不到cloneDeep

--- packages/components/FilterTable/src/index.vue
+++ packages/components/FilterTable/src/index.vue
@@ -552,7 +552,7 @@ let exSlot = {
     return data.props.render(h, params);
   }
 }
-import { cloneDeep } from 'lodash/cloneDeep'
+import { cloneDeep } from 'lodash'
 export default {
   name: "KdFilterTable",
   components: {
