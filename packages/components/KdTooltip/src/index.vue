86192909846e5d1c2e071e00b4f52d43e9d3cbb6
lizehui <lzh18235975914@163.com>
2022-08-12 20:17:51 +0800

    OTHER:优化tooltip组件

--- packages/components/KdTooltip/src/index.vue
+++ packages/components/KdTooltip/src/index.vue
@@ -3,8 +3,7 @@
     class="item"
     effect="dark"
     :disabled="isShowTooltip"
-    :content="text"
-    :placement="kdPlacement"
+    :placement="$attrs.placement"
     v-bind="$attrs"
   >
     <span
@@ -13,7 +12,7 @@
       :style="{ maxWidth: width }"
       @mouseover="onMouseOver('str')"
     >
-      <span ref="str">{{ text }}</span>
+      <span ref="str">{{ $attrs.content }}</span>
     </span>
     <slot></slot>
 
@@ -26,22 +25,10 @@ export default {
   name: "KdTooltip",
   components: {},
   props: {
-    text: { // 浮框显示的问题
-      type: String,
-      default: () => {
-        return "";
-      },
-    },
     width: { // 超过此宽度省略号显示
       type: String,
       default: () => {
         return "100%";
-      },
-    },
-    kdPlacement:{ // 浮框显示的位置
-      type: String,
-      default: () => {
-        return "top";
       },
     },
     slotShow: { // 是否显示插槽
