557153fb9777286e00e60c86e742289efa772c62
刘力豪 <18311056454@sina.cn>
2022-09-16 21:02:06 +0800

    FEATURE:新增dateRange不能选择将来时间的代码

--- examples/src/views/dateRange/index.vue
+++ examples/src/views/dateRange/index.vue
@@ -1,35 +1,29 @@
 <template>
-  <kd-date-range
-    title="sssssss"
-    v-model="val"
-  />
+  <div>
+    <kd-date-range title="基础用法" v-model="val" />
+    <kd-date-range
+      class="mt"
+      title="只能选择过去的时间"
+      v-model="dateValue"
+      futureDisabled
+    />
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Index",
-  props: {
-
-  },
+  name: 'Index',
+  props: {},
   data() {
     return {
-      val: []
+      val: [],
+      dateValue: []
     };
   },
-  components: {
-  
-  },
-  created() {
-
-  },
-  mounted() {
-
-  },
-  methods: {
-
-  }
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {}
 };
 </script>
-<style scoped lang='scss'>
-    
-</style>
+<style scoped lang="scss"></style>
