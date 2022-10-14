f28f9f61eb86ac847787c13f62c08cb1b3b5d7ce
liuyun <248196244@qq.com>
2022-10-15 00:40:31 +0800

    feat(popover-button): 完善组件示例文档

--- examples/src/views/popoverButton/index.vue
+++ examples/src/views/popoverButton/index.vue
@@ -1,40 +1,77 @@
 <template>
-  <div class="wrap">
+  <div>
+    <h3>popover按钮</h3>
+    <div class="author">维护人：刘云</div>
+    <div class="sub-title">
+      弹框按钮组件，基于el-popover封装，适用于需要二次确认的交互操作场景。
+    </div>
+
+    <h5>基础用法</h5>
+    <kd-popover-button @confirm="onConfirm"></kd-popover-button>
+    <example-code :source="source1">
+      该组件默认是为删除操作设计的
+    </example-code>
+
+    <h5>popover配置</h5>
     <kd-popover-button
-      class="m-r-8"
-      :disabled="false"
-      :content-text="'是否确认要批量删除数据表？'"
-      @confirm="batchDel"
-      @cancel="onCancel"
-    ></kd-popover-button>
-    <kd-popover-button
-      class="m-r-8"
-      content-text="确定暂停数据读取吗？"
-      reference-text="暂停"
-      :popover-attrs="{title: '暂停'}"
-      :loading-confirm="true"
-      :confirm-text="stopLoading? '加载中': '确定'"
-      @confirm="stopRead"
-      @cancel="cancelStop"
+       reference-text="批量导出"
+       :popover-attrs="{title: '批量导出'}"
+       content-text="确定批量导出数据？"
+       @confirm="onConfirm"
     >
       <template #reference>
-        <i class="icon kj-iconfont icon-geshihua m-l-4 cp" />
+        <i class="el-icon-s-tools" />
       </template>
     </kd-popover-button>
+    <example-code :source="source1">
+      使用方式同el-popover；<br />
+      默认触发popover的元素是el-button，reference-text用来设置按钮文字，reference-btn-attrs用来配置button属性；<br />
+      如果触发popover的元素不是button，可通过插槽reference自定义（同el-popover）；<br />
+      popover-attrs 接收一个对象，对应el-popover 的属性配置；<br />
+      content-text对应el-popover的content，也可用默认插槽自定义；content-styles 接收一个对象，控制content-text的样式；
+    </example-code>
+
+    <h5>按钮配置</h5>
+    <kd-popover-button
+        content-text="确定暂停数据读取吗？"
+        :popover-attrs="{title: '是否暂停'}"
+        :confirm-text="stopLoading? '加载中': '暂停'"
+        cancel-text="不暂停"
+        loading-confirm
+        @confirm="stopRead"
+        @cancel="cancelStop"
+    >
+      <template #reference>
+        <i class="el-icon-s-tools" />
+      </template>
+    </kd-popover-button>
+    <example-code :source="source1">
+      confirm-text/cancel-text 分别控制确认/取消按钮的文字；<br />
+      dis-confirm用来控制确认按钮的disabled属性；loading-confirm 用来控制确认按钮的loading属性；
+    </example-code>
   </div>
 </template>
 
 <script>
+import ExampleCode from "../../components/ExampleCode";
+import source1 from "./source/source1.md"
+import source2 from "./source/source2.md"
+import source3 from "./source/source3.md"
   export default {
     name: 'Index',
+    components: {
+      ExampleCode
+    },
     data() {
       return {
+        source1,
+        source2,
+        source3,
         stopLoading: false
       }
     },
     methods: {
-      batchDel() {
-        console.log('批量删除')
+      onConfirm() {
         this.$message.success('确认删除')
       },
       onCancel(){
@@ -53,3 +90,19 @@
     },
   }
 </script>
+<style lang="scss" scoped>
+.author{
+  margin-bottom:10px;
+  font-size:12px;
+  font-style: italic;
+}
+.sub-title{
+  font-size: 14px;
+  color: #5e6d82;
+  line-height: 1.5em;
+}
+.example-wrap{
+  width:600px;
+  margin-bottom:20px;
+}
+</style>
