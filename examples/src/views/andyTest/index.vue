a5f0d62adbb841e4672e36381580e96db01ce147
liulihao <551772063@qq.com>
2022-10-27 18:12:38 +0800

    FEATURE:修改uuid方法

--- examples/src/views/andyTest/index.vue
+++ examples/src/views/andyTest/index.vue
@@ -1,119 +1,78 @@
 <template>
-  <div class="box">
-    <el-form
-      ref="formRef"
-      :model="form"
-      label-position="right"
-      label-width="100px"
-    >
-      <el-form-item label="名称222" prop="name">
-        <br />
-        <kd-select
-          v-model="aa"
-          :options="options"
-          width="100"
-        />
-        <kd-select
-          v-model="aa"
-          :options="options"
-          popper-class="cc"
-        />
-      </el-form-item>
-    </el-form>
-    <div>下面的不在form中</div>
-    <kd-select v-model="aa" :options="options" />
-    <kd-select v-model="aa" :options="options" width="600" />
-    <div>hah a</div>
-    <kd-input v-model="aa" width="100%"></kd-input>
-    <kd-select v-model="aa" :options="options" />
-    <kd-select v-model="aa" :options="options" width="316" />
-
-    <kd-filter-table ref="tableRef" :data="tableData" :columns="columnsData">
-      <template #status="{ row }">
-        <kd-select v-model="aa" :options="options" width="700" />
-      </template>
-    </kd-filter-table>
+  <div>
+    <el-button @click="t1" type="primary">测试1</el-button>
+    <el-button @click="t2" type="info">测试2</el-button>
   </div>
 </template>
 
 <script>
+import { uuid } from "@kd/utils";
 export default {
-  name: "Test2",
+  name: "Index",
   components: {},
   props: {},
   data() {
     return {
-      tableData: [{ name: "andy", status: 1 }],
-      options: [
-        { label: "小泽泽", value: 18 },
-        { label: "小悦悦", value: 19 },
-        { label: "小鑫鑫", value: 20 }
-      ],
-      aa: 18,
-      form: {
-        name: "aa"
-      }
+      num: 123,
+      isShow: false,
+      str: "hello world",
+      arr: [11, 22, 33],
+      obj: {
+        name: "andy",
+        age: 18,
+        objArr: [{ chName: "凌云", height: 188 }]
+      },
+      date: new Date()
     };
   },
-  computed: {
-    columnsData() {
-      return [
-        {
-          type: "selection",
-          selectableFn: (row, index) => {
-            return index % 2 === 0;
-          }
-        },
-        {
-          title: "任务英文名称",
-          key: "name",
-          filters: [
-            { text: "是", value: 1 },
-            { text: "否", value: 0 }
-          ],
-          handler: this.deleteRow
-        },
-        {
-          title: "任务状态",
-          key: "status",
-          useSlot: true
-        },
-        {
-          key: "operation",
-          title: "操作",
-          btns: [
-            {
-              content: "编辑",
-              disabled: (item) => {
-                return this.mDisabled("UPDATE", item) || item.status === "on";
-              },
-              handler: this.deleteRow
-            },
-            {
-              content: "删除",
-              disabled: (item) => {
-                return this.mDisabled("DELETE", item) || item.status === "on";
-              },
-              confirm: this.deleteRow,
-              confirmInfo: "您确定要删除此文件嘛? "
-            }
-          ]
-        }
-      ];
-    }
-  },
+  computed: {},
   watch: {},
-  created() {},
+  created() {
+    this.t1();
+  },
   mounted() {},
   methods: {
-    deleteRow() {
-      console.log("deleteRow");
+    t1() {
+      console.log(
+        `%c 111=>36行 examples/src/views/andyTest/index.vue uuid() `,
+        "background:#000;color:#bada55",
+        uuid("我只能")
+      );
+      console.log(
+        `%c 222=>37行 examples/src/views/andyTest/index.vue uuid('time') `,
+        "background:#000;color:#bada55",
+        uuid("time")
+      );
+      console.log(
+        `%c 222=>37行 examples/src/views/andyTest/index.vue uuid('time') `,
+        "background:#000;color:#bada55",
+        uuid("phone")
+      );
+      console.log(
+        `%c 222=>37行 examples/src/views/andyTest/index.vue uuid('time') `,
+        "background:#000;color:#bada55",
+        uuid("email")
+      );
+      uuid([
+        { label: "小泽泽", value: "xzz" },
+        { label: "小月月", value: "xyy" }
+      ]);
+      console.log(
+        ` uuid([ { label: "小泽泽", value: "xzz" },{ label: "小月月", value: "xyy" }])`,
+        uuid([
+          { label: "小泽泽", value: "xzz" },
+          { label: "小月月", value: "xyy" }
+        ])
+      );
+      console.log(
+        uuid("time", 0, { startStr: "andy", timeStr: "{h}:{i}:{s}" })
+      );
+      console.log(uuid('number'), typeof uuid('number'));
+    },
+    t2() {
+      console.log("t2");
     }
   }
 };
 </script>
-<style scoped lang="scss">
-.cc {
-  width: 100px !important;
-}
-</style>
+<style scoped lang="scss"></style>
